b25a47ad190cf4e461dad4e80d6e8e549e8c2763
xatkx <xatkx1@gmail.com>
2018-07-01 07:53:04 -0400

    mas planetas y una luna

--- script/script.js
+++ script/script.js
@@ -15,36 +15,79 @@ button.addEventListener("click", algo);
 
 
 function algo () {
+   
+ 
 
-
-	var g_marte = 3.7;
 	var g_tierra = 9.8;
-	var g_jupyter = 24.8;
+	var mercurio = 2.78;
+	var venus = 8.87;
+	var luna = 1.62;
+	var marte = 3.72;
+	var jupiter = 22.88;
+	var saturno = 9.05;
+	var urano = 7.77; 
+	var neptuno = 11;
+	var pluton = 0.4;
+
 
 	var personaPeso = parseInt(peso.value);
+	console.log(personaPeso);
 	var pesoFinal;
 
-if (option.value == "marte") {
-	
-	pesoFinal = (personaPeso * g_marte) / g_tierra;	
-	pesoFinal = parseInt(pesoFinal);
+//------------------------------------------
+	switch (option.value) {
+	case "marte":
+		pesoFinal = (personaPeso * marte) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+		break;
+	case "jupyter":
+		pesoFinal = (personaPeso * jupiter) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+	case "mercurio":
+		pesoFinal = (personaPeso * mercurio) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+		break;
+	case "venus":
+		pesoFinal = (personaPeso * venus) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+	case "luna":
+		pesoFinal = (personaPeso * luna) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+		break;
+	case "saturno":
+		pesoFinal = (personaPeso * saturno) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+	case "urano":
+		pesoFinal = (personaPeso * urano) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+		break;
+	case "neptuno":
+		pesoFinal = (personaPeso * neptuno) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+	case "pluton":
+		pesoFinal = (personaPeso * pluton) / g_tierra;
+		pesoFinal = parseInt(pesoFinal);
+		result.value = pesoFinal+" kilos";
+		break;
 
-	result.value = pesoFinal+" kilos";
-}else if(option.value == "jupyter") {
-	
-	pesoFinal = (personaPeso * g_jupyter) / g_tierra;
-	pesoFinal = parseInt(pesoFinal);
-
-	result.value = pesoFinal+" kilos";
-		
+	default:
+		// statements_def
+		result.value = "paso 2 please";
+		break;
 }
-else {
-	result.value = "paso 2 please";
-}
+
+
 
 
 }
-
 
 
 
